e1533b77b22c9c05e797c9e0786128ae9361e61d
murad <muradkv@gmail.com>
2019-04-25 22:08:45 +0300

    Show on a tableView usedWords

--- Word Scramble/TableViewController.swift
+++ Word Scramble/TableViewController.swift
@@ -38,7 +38,15 @@ class TableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        
+        return cell
     }
     
     func startGame() {
