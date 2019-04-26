3510ffc288c40a44baf466bc28f1c956420fd645
murad <muradkv@gmail.com>
2019-04-26 22:34:46 +0300

    User get feedback if word is invalid

--- Word Scramble/TableViewController.swift
+++ Word Scramble/TableViewController.swift
@@ -74,6 +74,9 @@ class TableViewController: UITableViewController {
     func submit(_ answer: String) {
         let lowerAnswer: String = answer.lowercased()
         
+        let errorTitle: String
+        let errorMessage: String
+        
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
@@ -83,9 +86,24 @@ class TableViewController: UITableViewController {
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
                     
+                    return
+                } else {
+                    errorTitle = "Word not recognised"
+                    errorMessage = "You can't just make them up, you know!"
                 }
+            } else {
+                errorTitle = "Word used already"
+                errorMessage = "Be more original!"
             }
+        } else {
+            guard let title = title?.lowercased() else { return }
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title)"
         }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
 
     //Check input word whether all the letters in the array word
